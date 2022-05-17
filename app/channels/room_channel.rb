cc56ebe04bb90245316c25889e0a05818803b21d
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-05-17 20:34:58 +0900

    room_channnel.rbを修正

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,7 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     stream_from 'room_channel'
+  end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
@@ -10,11 +11,7 @@ class RoomChannel < ApplicationCable::Channel
     user = User.find_by(email: data['email'])
 
     if message = Message.create(content: data['message'], user_id: user.id)
-      ActionCable.server.bloadcast('room_channel', {
-        message: data['message'],
-        name: user.name,
-        created_at: message.created_at
-      })
+      ActionCable.server.broadcast 'room_channel', { message: data['message'], name: user.name, created_at: message.created_at }
     end
   end
 end
