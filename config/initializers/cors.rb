26c1390fc6f3a4b4dd9b50695cc2a237a7373303
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-05-19 15:20:57 +0900

    cors.rbの編集

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins ENV['API_DOMAIN'] || ''
-    origins '*'
+    origins ENV['API_DOMAIN'] || ''
+    # origins '*'
 
     resource '*',
       headers: :any,
